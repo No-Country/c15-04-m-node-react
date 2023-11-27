0cf8b33c3759b0a860791733e558e5ecac85dad2
Mike2020x <62442141+Mike2020x@users.noreply.github.com>
2023-11-27 15:14:01 -0500

    changes added

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,26 +1,7 @@
 import "./App.css";
-import { Link, Route, Routes } from "react-router-dom";
-
-import Home from "./pages/Home";
-import Contact from "./pages/Contact";
 
 function App() {
-	return (
-		<div className="container">
-			<nav>
-				<ul className="flex gap-2">
-					<Link to="/">Home</Link>
-					<Link to="/contact">Contact</Link>
-				</ul>
-			</nav>
-
-			{/* Defining routes path and rendering components as element */}
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/contact" element={<Contact />} />
-			</Routes>
-		</div>
-	);
+	return <div className="container"></div>;
 }
 
 export default App;
--- front/src/routes/index.tsx
+++ front/src/routes/index.tsx
@@ -1,3 +1,16 @@
-const routes = [{}];
+import { createBrowserRouter } from "react-router-dom";
+import Home from "@/pages/Home";
+import Contact from "@/pages/Contact";
 
-export default routes;
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Home />,
+	},
+	{
+		path: "/contact",
+		element: <Contact />,
+	},
+]);
+
+export default router;
