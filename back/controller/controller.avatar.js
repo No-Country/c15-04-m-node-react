4f4a37dce503f669690d0debd73e418988a8e7f3
Alejandro Quiñones <alejandro.quinones@outlook.es>
2023-11-27 23:54:31 -0300

    avatar Schema added - get avatar router created

---
+++ back/controller/controller.avatar.js
@@ -0,0 +1,18 @@
+const Avatar = require('../models/avatars')
+const { response } = require('express')
+require('colors')
+
+const getAvatars = async ( _, res = response) => {
+    try {
+        const avatars = await Avatar.find({})
+        res.status(200).json({ avatars })
+    } catch(e) {
+        console.log('ERROR!'.red, e.message)
+        res.status(500).json({
+            message: 'No fué posible mostrar los avatars',
+            error: e.message
+        })
+    }
+}
+
+module.exports = getAvatars
