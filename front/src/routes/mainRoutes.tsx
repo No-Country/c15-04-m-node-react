a9222bccf6843086ce7572a91f956f520fe2262c
lucenstuff <lucentiniagustin@hotmail.com>
2023-11-28 23:30:07 -0300

    added favicon, added loading screen (check on endpoint /loadingscreen)

--- front/src/routes/mainRoutes.tsx
+++ front/src/routes/mainRoutes.tsx
@@ -1,10 +1,10 @@
 import Home from "@/pages/Home";
 import { RouteObject } from "react-router-dom";
-
 import Contact from "@/pages/Contact/Contact";
 import MainLayout from "@/layout/MainLayout/MainLayout";
 import SignIn from "@/pages/SignIn";
 import Login from "@/pages/Login";
+import LoadinScreen from "@/components/LoadinScreen/LoadinScreen";
 
 const mainRoutes: RouteObject = {
 	element: <MainLayout />,
@@ -24,6 +24,10 @@ const mainRoutes: RouteObject = {
 		{
 			path: "/login",
 			element: <Login />,
+		},
+		{
+			path: "/loadingscreen",
+			element: <LoadinScreen />,
 		},
 	],
 };
