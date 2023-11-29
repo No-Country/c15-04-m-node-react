cf86fcf6245465dc86ebfc42efa6b0e3fafd6d8b
Sanchez-PC\Sanchez <brandon2haru@gmail.com>
2023-11-29 12:24:52 -0400

    Carpeta base de las utilidades para la huella de carbono en Helpers, agregado los factores de emision por paises actualizados 2023, agregado validacion para los campos requeridos de la huella de carbono del consumo electrico, controlador para el calculo de la huella de carbono, ruta para testear el calculo de la huella de carbono

--- back/routes/routes.usuario.js
+++ back/routes/routes.usuario.js
@@ -5,13 +5,14 @@ const {
     update, 
     eliminar
 } = require('../controller/controller.usuario')
-const { POST_LOGIN, POST_SIGN_UP, TOKEN } = require('../helpers/checks')
-
+const { getElectricityCFP } = require('../controller/controller.carbonfp')
+const { POST_LOGIN, POST_SIGN_UP, TOKEN, ECFP_VALIDATION } = require('../helpers/checks')
 const router = Router()
 
 router.post('/sign-up', POST_SIGN_UP, signUp)
 router.post('/login', POST_LOGIN, logIn)
 router.put('/actualizar', TOKEN, update)
 router.delete('/eliminar', TOKEN, eliminar)
+router.post('/test', ECFP_VALIDATION, getElectricityCFP)
 
 module.exports = router
