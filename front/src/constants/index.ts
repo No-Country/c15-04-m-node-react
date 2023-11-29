94b20d9f8e9429c9deb6aed44819f019e2129ad8
Santiago Ariel <santiagoarielv98@gmail.com>
2023-11-29 19:13:09 -0300

    Update API_URL and add UserProvider component

--- front/src/constants/index.ts
+++ front/src/constants/index.ts
@@ -1,3 +1,3 @@
 export const GlobalConstants = {
-	API_URL: process.env.VITE_API_URL || "http://localhost:8080/api",
+	API_URL: import.meta.env.VITE_API_URL || "http://localhost:8080/api",
 };
--- front/src/pages/Signup.tsx
+++ front/src/pages/Signup.tsx
@@ -2,7 +2,7 @@ import RegisterForm from "@/components/Register/RegisterForm";
 const Signup = () => {
 	return (
 		<>
-			<div class="fixed top-0 left-0 flex items-center justify-center w-screen h-screen">
+			<div className="fixed top-0 left-0 flex items-center justify-center w-screen h-screen">
 				<RegisterForm />
 			</div>
 		</>
