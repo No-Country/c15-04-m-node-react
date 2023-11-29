86ef81926e6f6d7f397e1f87b584979e83f05681
lucenstuff <lucentiniagustin@hotmail.com>
2023-11-29 20:31:27 -0300

    added hero section, improvements in navbar and video card

--- front/src/components/Footer/Footer.tsx
+++ front/src/components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
 	return (
-		<footer className="bg-green-500 text-white py-4 w-full fixed bottom-0">
+		<footer className="bg-green-500 text-white py-4 w-full">
 			<div className="container mx-auto text-center">
 				<p>&copy; 2023 Your Company. All rights reserved.</p>
 			</div>
--- front/src/components/Landing/Landing.tsx
+++ front/src/components/Landing/Landing.tsx
@@ -1,15 +1,19 @@
 import Footer from "../Footer/Footer";
+import HeroSection from "../ui/Landing/HeroSection";
 import Navbar from "../Navbar/Navbar";
 import VideoCard from "../ui/Landing/VideoCard";
+import Presentation from "../ui/Landing/Presentation";
 
 const Landing = () => {
 	return (
 		<div className="landing-container">
 			<Navbar />
-			<body>
+			<body className="flex flex-col items-center ">
+				<HeroSection />
+				<Presentation />
 				<VideoCard />
+				<Footer />
 			</body>
-			<Footer />
 		</div>
 	);
 };
