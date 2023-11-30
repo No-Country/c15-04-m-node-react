cab5e7ca9e8083044040babe217e81fc3e678d38
lucenstuff <lucentiniagustin@hotmail.com>
2023-11-30 11:59:23 -0300

    improved footer responsivity

--- front/src/components/Footer/Footer.tsx
+++ front/src/components/Footer/Footer.tsx
@@ -10,25 +10,25 @@ const Footer = () => {
 					<p className="py-2">Argentina</p>
 					<p className="py-2">hello@greentrace.com</p>
 				</div>
-				<p className="py-5">&copy; 2023 Soluciones de tecnologías climáticas</p>
 			</div>
-			<div className="container mx-auto text-center">
+			<div className="container mx-auto text-center md:flex md:gap-4 md:justify-between">
+				<p className="py-5">&copy; 2023 Soluciones de tecnologías climáticas</p>
 				<p className="py-5">Política de privacidad</p>
 				<p className="py-5">Términos de uso</p>
-			</div>
-			<div className="flex justify-center gap-4 py-20">
-				<a href="#">
-					<img src="./src/assets/img/socials/linkedin.svg" alt="linkedin" width={"40x"} />
-				</a>
-				<a href="#">
-					<img src="./src/assets/img/socials/facebook.svg" alt="facebook" width={"40x"} />
-				</a>
-				<a href="#">
-					<img src="./src/assets/img/socials/twitter.svg" alt="twitter" width={"40px"} />
-				</a>
-				<a href="#">
-					<img src="./src/assets/img/socials/instagram.svg" alt="instagram" width={"40px"} />
-				</a>
+				<div className="flex justify-center gap-4 py-20 md:py-0 md:px-10 m">
+					<a href="#">
+						<img src="./src/assets/img/socials/linkedin.svg" alt="linkedin" width={"40x"} />
+					</a>
+					<a href="#">
+						<img src="./src/assets/img/socials/facebook.svg" alt="facebook" width={"40x"} />
+					</a>
+					<a href="#">
+						<img src="./src/assets/img/socials/twitter.svg" alt="twitter" width={"40px"} />
+					</a>
+					<a href="#">
+						<img src="./src/assets/img/socials/instagram.svg" alt="instagram" width={"40px"} />
+					</a>
+				</div>
 			</div>
 		</footer>
 	);
