e32369079f796cd58a28b3fec6091b845b604baa
lucenstuff <lucentiniagustin@hotmail.com>
2023-12-01 12:13:08 -0300

    added Newsletter card

--- front/src/components/ui/Landing/NewsLetter.tsx
+++ front/src/components/ui/Landing/NewsLetter.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 
 const NewsLetter: React.FC = () => {
-	const [name, setName] = useState<string>("");
-	const [email, setEmail] = useState<string>("");
+	const [name, setName] = useState<string>("Nombre");
+	const [email, setEmail] = useState<string>("Email");
 
 	const handleSubmit = (e: React.FormEvent) => {
 		e.preventDefault();
@@ -12,10 +12,12 @@ const NewsLetter: React.FC = () => {
 
 	return (
 		<div className="px-6 py-2 md:w-3/4 ">
-			<div className="md:flex bg-white shadow-lg rounded-lg p-4 x border border-gray-200 text-center flex-col md:flex-row">
-				<div className="md:flex-row justify-between w-25">
-					<form onSubmit={handleSubmit} className=" p-4 rounded-lg flex flex-col items-center">
-						<h2 className="text-xl font-bold mb-4">Subscribete a nuestro newsletter</h2>
+			<div className="flex justify-center  bg-white shadow-lg rounded-lg p-4 x border border-gray-200 text-center md:flex-rowd">
+				<div className="md:flex-row w-full md:w-3/4">
+					<h2 className="text-xl font-bold mb-4 px-10 pt-4 md:text-2xl">
+						Subscribete a nuestro <br /> newsletter
+					</h2>
+					<form onSubmit={handleSubmit} className=" p-4 flex flex-col items-center md:flex-row md:gap-4">
 						<div className="mb-4 border border-gray-200 shadow-lg rounded-lg text-slate-400 gap-10 w-full">
 							<input
 								type="text"
@@ -36,7 +38,7 @@ const NewsLetter: React.FC = () => {
 						</div>
 						<div className="mb-4 border text-emerald-500 border-emerald-500 rounded-lg shadow-lg gap-10 w-1/2 ">
 							<button type="submit" className=" text-start px-4 py-2 rounded-r-md">
-								Subscribe
+								Subscribirse
 							</button>
 						</div>
 					</form>
