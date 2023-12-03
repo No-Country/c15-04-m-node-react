a164612cf990aaa7c62d3329dafde7fb98cf03f6
Alejandro Quiñones <alejandro.quinones@outlook.es>
2023-12-02 23:17:20 -0300

    adjusting values on transport calculator

--- back/helpers/emissions/transport/colectivo.js
+++ back/helpers/emissions/transport/colectivo.js
@@ -1,5 +1,5 @@
 const car = require('./car')
 
-const colectivoEmission = (kms = 0) => Number((car(kms, 'medium') / 4).toFixed(2))
+const colectivoEmission = (kms = 0) => Number((car(kms, 'medium') / 3.2).toFixed(2))
 
 module.exports = colectivoEmission
--- back/helpers/emissions/transport/motocycle.js
+++ back/helpers/emissions/transport/motocycle.js
@@ -1,6 +1,6 @@
 const motocycleEmission = (kms = 0) => {
     const kgCoPerLt = 2.31
-    const kmPerLt = 16
+    const kmPerLt = 15.8
     const result = (kms / kmPerLt) * kgCoPerLt
     return Number(result.toFixed(2))
 }
