5b277a126956eca3da01fb764ba95bd93549fefd
Sanchez-PC\Sanchez <brandon2haru@gmail.com>
2023-12-04 10:53:48 -0400

    anexado funciones para el controlador de la calculadora de la huella de carbono

--- back/controller/controller.carbonfp.js
+++ back/controller/controller.carbonfp.js
@@ -1,7 +1,7 @@
 const { request, response } = require('express')
 const carbonFP = require('../helpers/carbon_footprint')
 
-const getElectricityCFP = (req = request, res = response) => {
+const electricityCalculator = (req = request, res = response) => {
 	const { electricidad } = req.body;
 
 	const carbon_footprint = carbonFP.getElectricity(electricidad);
@@ -13,4 +13,29 @@ const getElectricityCFP = (req = request, res = response) => {
     })
 }
 
-module.exports = { getElectricityCFP }
+const gasCalculator = (req = request, res = response) => {
+	const { gas } = req.body;
+
+	const carbon_footprint = carbonFP.getGas(gas);
+	const carbonOffset = carbonFP.getCarbonOffset(carbon_footprint);
+
+	res.status(201).json({
+            carbon_footprint,
+            carbonOffset
+    })
+}
+
+const carbonOffsetCalculator = (req = request, res = response) => {
+	const { huella_carbono } = req.body;
+	const carbonOffset = carbonFP.getCarbonOffset(huella_carbono);
+
+	res.status(201).json({
+            carbonOffset
+    })
+}
+
+module.exports = { 
+	electricityCalculator,
+	gasCalculator,
+	carbonOffsetCalculator 
+}
