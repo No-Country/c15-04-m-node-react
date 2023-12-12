15026418b7e7a078ceb839e8e7b0d87c1205161a
Mike2020x <62442141+Mike2020x@users.noreply.github.com>
2023-12-12 18:15:45 -0500

    add questions

--- front/src/components/FormSlider/TransportSlider.tsx
+++ front/src/components/FormSlider/TransportSlider.tsx
@@ -14,105 +14,256 @@ const TransportSlider = () => {
 		},
 	};
 	const options1 = [
-		{ label: "Si", value: "true" },
-		{ label: "No", value: "false" },
-	];
-	const options2 = [
-		{ label: "ida", value: "false" },
-		{ label: "ida y vuelta", value: "true" },
+		{
+			name: "residence",
+			title: "¿En qué país reside?",
+			type: "text",
+		},
+		{
+			name: "transport",
+			title: "¿Utiliza el transporte público?",
+			type: "radio",
+			options: [
+				{ value: "yes", label: "Si" },
+				{ value: "no", label: "No" },
+			],
+			conditions: [
+				{
+					triggerAnswer: "yes",
+					questions: [
+						{
+							name: "transport.public",
+							title: "¿Cuántas veces utiliza el transporte público?",
+							type: "number",
+						},
+						{
+							name: "transport.bus",
+							title: "¿Cuántos kilómetros ha recorrido usando el bus?",
+							type: "number",
+						},
+						{
+							name: "transport.taxi",
+							title: "¿Cuántos kilómetros ha recorrido usando taxi?",
+							type: "number",
+						},
+						{
+							name: "transport.metro",
+							title: "¿Cuántos kilómetros ha recorrido usando el metro?",
+							type: "number",
+						},
+					],
+				},
+			],
+		},
+		{
+			name: "vehicle",
+			title: "¿Posee vehículo personal, alquilado o prestado?",
+			type: "radio",
+			options: [
+				{ value: "yes", label: "Si" },
+				{ value: "no", label: "No" },
+			],
+			conditions: [
+				{
+					triggerAnswer: "yes",
+					questions: [
+						{
+							name: "vehicle.car",
+							title: "¿Posee automóvil?",
+							type: "radio",
+							options: [
+								{ value: "yes", label: "Si" },
+								{ value: "no", label: "No" },
+							],
+							conditions: [
+								{
+									triggerAnswer: "yes",
+									questions: [
+										{
+											name: "vehicle.car.kilometers",
+											title: "¿Cuántos kilómetros recorre en promedio en automóvil?",
+											type: "number",
+										},
+										{
+											name: "vehicle.car.size",
+											title: "¿De que tamaño considera es su automóvil?",
+											type: "select",
+											options: [
+												{ value: "small", label: "Pequeño" },
+												{ value: "medium", label: "Mediano" },
+												{ value: "large", label: "Grande" },
+											],
+										},
+										{
+											name: "vehicle.car.fuel",
+											title: "¿Qué tipo de combustible consume su automóvil?",
+											type: "select",
+											options: [
+												{ value: "gasoline", label: "Gasolina" },
+												{ value: "diesel", label: "Diesel" },
+												{ value: "electric", label: "Eléctrico" },
+											],
+										},
+									],
+								},
+							],
+						},
+						{
+							name: "vehicle.motorcycle",
+							title: "¿Posee motocicleta?",
+							type: "radio",
+							options: [
+								{ value: "yes", label: "Si" },
+								{ value: "no", label: "No" },
+							],
+							conditions: [
+								{
+									triggerAnswer: "yes",
+									questions: [
+										{
+											name: "vehicle.motorcycle.kilometers",
+											title: "¿Cuántos kilómetros recorre en promedio en motocicleta?",
+											type: "number",
+										},
+									],
+								},
+							],
+						},
+					],
+				},
+			],
+		},
+		{
+			name: "bike",
+			title: "¿Hace uso de bicicletas/monopatines para trasladarse a lugares donde lleva a cabo actividades diarias?",
+			type: "radio",
+			options: [
+				{ value: "yes", label: "Si" },
+				{ value: "no", label: "No" },
+			],
+			conditions: [
+				{
+					triggerAnswer: "yes",
+					questions: [
+						{
+							name: "bike.kilometers",
+							title: "¿Cuántas veces haces uso de estos?",
+							type: "number",
+						},
+					],
+				},
+			],
+		},
+		{
+			name: "walk",
+			title: "¿Has caminado algunas veces hasta tu lugar de trabajo o donde llevas a cabo actividades cotidianas?",
+			type: "radio",
+			options: [
+				{ value: "yes", label: "Si" },
+				{ value: "no", label: "No" },
+			],
+			conditions: [
+				{
+					triggerAnswer: "yes",
+					questions: [
+						{
+							name: "walk.kilometers",
+							title: "¿Cuántos metros caminas?",
+							type: "number",
+						},
+					],
+				},
+			],
+		},
+		{
+			name: "flight",
+			title: "¿Ha realizado algún viaje en avión hacia otro estado, país o continente?",
+			type: "radio",
+			options: [
+				{ value: "yes", label: "Si" },
+				{ value: "no", label: "No" },
+			],
+			conditions: [
+				{
+					triggerAnswer: "yes",
+					questions: [
+						{
+							name: "flight.kilometers",
+							title: "¿De cuantos kilómetros fue el viaje?",
+							type: "number",
+						},
+						{
+							name: "flight.seats",
+							title: "¿Qué cantidad de asientos ocupó?",
+							type: "number",
+						},
+						{
+							name: "flight.round",
+							title: "¿Su viaje consistió en ida y vuelta o solo ida?",
+							type: "select",
+							options: [
+								{ value: "round", label: "Ida y vuelta" },
+								{ value: "one-way", label: "Solo ida" },
+							],
+						},
+					],
+				},
+			],
+		},
+		{
+			name: "gas",
+			title: "¿Ocupa el uso de gas doméstico?",
+			type: "radio",
+			options: [
+				{ value: "yes", label: "Si" },
+				{ value: "no", label: "No" },
+			],
+			conditions: [
+				{
+					triggerAnswer: "yes",
+					questions: [
+						{
+							name: "gas.type",
+							title: "¿Qué tipo de gas consume?",
+							type: "select",
+							options: [
+								{ value: "natural", label: "Natural" },
+								{ value: "propane", label: "Propano" },
+							],
+						},
+						{
+							name: "gas.units",
+							title: "¿En qué unidad de medida está cuantificada su consumo de gas?",
+							type: "select",
+							options: [
+								{ value: "kg", label: "Kilogramos" },
+								{ value: "m3", label: "Metros cúbicos" },
+							],
+						},
+					],
+				},
+			],
+		},
+		{
+			name: "electricity.consumption",
+			title: "¿De acuerdo a su factura eléctrica, cuántos kWh (kilovatios/hora) consumió el ultimo mes?",
+			type: "number",
+		},
+		{
+			name: "project",
+			title: "¿Ha llevado a cabo últimamente algún proyecto en pro del medio ambiente?",
+			type: "radio",
+			options: [
+				{ value: "yes", label: "Si" },
+				{ value: "no", label: "No" },
+			],
+		},
 	];
 
 	const handleInputSubmit = (value: number) => {
 		console.log("Respuesta enviada:", value);
 	};
 
-	return (
-		<div className=" bg-white-500 h-screen flex items-center justify-center">
-			<Swiper pagination={pagination} modules={[Pagination]} className="w-3/4 h-3/5 flex rounded-lg">
-				<SwiperSlide className=" bg-white h-20">
-					<h3 className="text-center m-4 text-black text-2xl">
-						¿Utiliza el transporte público? <hr />
-					</h3>
-					<ButtonQuestions options={options1} onClick={(option) => console.log(option)} />
-				</SwiperSlide>
-
-				<SwiperSlide className=" bg-white h-20">
-					<h3 className="text-center m-4 text-black text-2xl">
-						¿Cuántas veces utiliza el transporte público?
-						<hr />
-					</h3>
-					<InputQuestions onSubmit={handleInputSubmit} />
-				</SwiperSlide>
-				<SwiperSlide className=" bg-white h-20">
-					<h3 className="text-center m-4 text-black text-2xl">
-						¿Cuántos kilómetros ha recorrido usando el bus? <hr />{" "}
-						<small className="text-lg">
-							Si no conoce la cifra exacta, puede ingresar una estimación que usted crea sea el valor más aproximado.
-						</small>
-					</h3>
-					<InputQuestions onSubmit={handleInputSubmit} />
-				</SwiperSlide>
-				<SwiperSlide className=" bg-white h-20">
-					<h3 className="text-center m-4 text-black text-2xl">
-						¿Cuántos kilómetros ha recorrido usando taxi o auto? <hr />{" "}
-						<small className="text-lg">
-							Si no conoce la cifra exacta, puede ingresar una estimación que usted crea sea el valor más aproximado.
-						</small>
-					</h3>
-					<InputQuestions onSubmit={handleInputSubmit} />
-				</SwiperSlide>
-				<SwiperSlide className=" bg-white h-20">
-					<h3 className="text-center m-4 text-black text-2xl">
-						¿Cuántos kilómetros ha recorrido usando el metro? <hr />{" "}
-						<small className="text-lg">
-							Si no conoce la cifra exacta, puede ingresar una estimación que usted crea sea el valor más aproximado.
-						</small>
-					</h3>
-					<InputQuestions onSubmit={handleInputSubmit} />
-				</SwiperSlide>
-				<SwiperSlide className=" bg-white h-20">
-					<h3 className="text-center m-4 text-black text-2xl">
-						¿Cuántos kilómetros ha recorrido usando taxi o auto? <hr />{" "}
-						<small className="text-lg">
-							Si no conoce la cifra exacta, puede ingresar una estimación que usted crea sea el valor más aproximado.
-						</small>
-					</h3>
-					<InputQuestions onSubmit={handleInputSubmit} />
-				</SwiperSlide>
-				<SwiperSlide className=" bg-white h-20">
-					<h3 className="text-center m-4 text-black text-2xl">
-						¿Ha realizado algún viaje en avión hacia otro estado, país o continente?
-						<hr />
-					</h3>
-					<ButtonQuestions options={options1} onClick={(option) => console.log(option)} />
-				</SwiperSlide>
-
-				<SwiperSlide className=" bg-white h-20">
-					<h3 className="text-center m-4 text-black text-2xl">
-						¿Qué cantidad de asientos ocupó?
-						<hr /> <small className="text-lg">Si viajó en compañía, ingrese la cantidad ocupada.</small>
-					</h3>
-					<InputQuestions onSubmit={handleInputSubmit} />
-				</SwiperSlide>
-				<SwiperSlide className=" bg-white h-20">
-					<h3 className="text-center m-4 text-black text-2xl">
-						¿De cuantos kilómetros fue el viaje?
-						<hr />
-					</h3>
-					<small className="text-lg">
-						Si no conoce la cifra exacta, puede ingresar una estimación que usted crea sea el valor más aproximado.
-					</small>
-					<InputQuestions onSubmit={handleInputSubmit} />
-				</SwiperSlide>
-				<SwiperSlide className=" bg-white h-20">
-					<h3 className="text-center m-4 text-black text-2xl">
-						¿Su viaje consistió en ida y vuelta o solo ida?
-						<hr />
-					</h3>
-					<ButtonQuestions options={options2} onClick={(option) => console.log(option)} />
-				</SwiperSlide>
-			</Swiper>
-		</div>
-	);
+	return <div className=" bg-white-500 h-screen flex items-center justify-center"></div>;
 };
 export default TransportSlider;
