3326350e949bf997861dc9938a1df8e1a0fc7acb
Santiago Ariel <santiagoarielv98@gmail.com>
2023-12-19 14:16:42 -0300

    Add react-icons package and Footer component

---
+++ front/src/components/Footer/items.ts
@@ -0,0 +1,40 @@
+import { BsTwitterX, BsFacebook, BsInstagram } from "react-icons/bs";
+import { FaLinkedinIn } from "react-icons/fa";
+
+export const items = [
+	{
+		name: "Argentina",
+		img: "https://flagcdn.com/ar.svg",
+	},
+	{
+		name: "Chile",
+		img: "https://flagcdn.com/cl.svg",
+	},
+	{
+		name: "Venezuela",
+		img: "https://flagcdn.com/ve.svg",
+	},
+	{
+		name: "Colombia",
+		img: "https://flagcdn.com/co.svg",
+	},
+];
+
+export const icons = [
+	{
+		name: "facebook",
+		icon: BsFacebook,
+	},
+	{
+		name: "twitter",
+		icon: BsTwitterX,
+	},
+	{
+		name: "instagram",
+		icon: BsInstagram,
+	},
+	{
+		name: "linkedin",
+		icon: FaLinkedinIn,
+	},
+];
