b357e50fff200168fe5b3ed2b7851f652eeb37b4
Santiago Ariel <santiagoarielv98@gmail.com>
2023-12-20 10:04:52 -0300

    Update Footer and TeamSection components

--- front/src/components/Landing/TeamSection.tsx
+++ front/src/components/Landing/TeamSection.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { team } from "./users";
 
 const TeamSection = () => {
@@ -10,21 +11,21 @@ const TeamSection = () => {
 				</div>
 				<div className="flex flex-wrap -m-4">
 					{team.map((user) => (
-						<div className="p-2 lg:w-1/3 md:w-1/2 text-center  mx-auto">
-							<div className="p-6 rounded-lg max-w-sm">
+						<div className="p-2 lg:w-1/3 md:w-1/2 text-center mx-auto">
+							<div className="p-6 rounded-lg max-w-sm bg-slate-100 dark:bg-slate-900">
 								<img
 									className="rounded-full w-full h-full object-cover object-center mb-6 max-w-[60%] mx-auto aspect-square"
 									src={user.img}
 									alt={user.name}
 								/>
 								<h3 className="text-lg font-medium">{user.name}</h3>
-								<span className="text-gray-500 mb-3 block">{user.role}</span>
-								<p className="text-gray-600 text-sm">{user.description}</p>
+								<span className="text-gray-500 mb-3 block dark:text-gray-400">{user.role}</span>
+								<p className="text-gray-600 text-sm dark:text-gray-500">{user.description}</p>
 								<div className="flex items-center justify-center mt-4">
 									{user.social.map((social, index) => (
-										<a href={social.url} key={index} className="mx-2" target="_blank" rel="noopener noreferrer">
-											<social.icon className="w-5 h-5 text-gray-400" />
-										</a>
+										<Link to={social.url} key={index} className="mx-2" target="_blank" rel="noopener noreferrer">
+											<social.icon className="w-5 h-5 fill-current dark:text-gray-400 dark:hover:text-gray-200 transition-colors text-gray-500 hover:text-gray-900" />
+										</Link>
 									))}
 								</div>
 							</div>
