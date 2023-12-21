da66bcf0bae084cd8b225e3151fd59d05d4fdf06
lucenstuff <lucentiniagustin@hotmail.com>
2023-12-21 12:27:33 -0300

    working footprint page, must optimize

--- front/src/pages/Dashboard/FootprintPage.tsx
+++ front/src/pages/Dashboard/FootprintPage.tsx
@@ -3,16 +3,15 @@ import React from "react";
 import { Flame, Plug, Car, Plane } from "lucide-react";
 import { useUserContext } from "@/hooks/useUserContext";
 import { getCarbonFootprint } from "@/services/userService";
-import { get } from "http";
 
-const gas = 25;
-const electricity = 25;
-const landTransport = 25;
-const plane = 25;
-const trees = 0;
-const carDistance = 0;
+let gas = 25;
+let electricity = 25;
+let landTransport = 25;
+let plane = 25;
+let trees = 0;
+let carDistance = 0;
 
-const data = {
+const graphData = {
 	labels: [],
 	datasets: [
 		{
@@ -24,13 +23,63 @@ const data = {
 	],
 };
 
-const options = {};
+interface OffsetItem {
+	minTrees: number;
+	message: string;
+}
+
+interface EquivalenceItem {
+	carDistance: number;
+	message: string;
+}
+
+interface CarbonFootprintData {
+	data: {
+		offset_by_user: any[]; // Assuming you don't have details on the structure
+		offsets: OffsetItem[];
+		statistics: {
+			carbon_footprint: {
+				electricity: number;
+				gas: string;
+				total: number;
+			};
+			transport: {
+				land: number;
+				air: number;
+				total: number;
+			};
+			emission_percentage: {
+				electricity_perc: number; // Assuming 0 is a number, not a string
+				gas_perc: string; // Changed from number to string
+				higher_emission: {
+					category: string;
+					value: string;
+				};
+				transport_perc: {
+					air_perc: number;
+					land_perc: number;
+					total: string;
+				};
+			};
+		};
+		equivalences: EquivalenceItem[]; // Changed from any[] to EquivalenceItem[]
+	};
+	message: string;
+}
 
 async function getData() {
-	const result = await getCarbonFootprint();
-	const emissionPercentage = result.carbonOffset.statistics.emission_percentage ?? 0;
-	console.log(emissionPercentage);
+	const result: CarbonFootprintData = await getCarbonFootprint();
+	gas = result.data.statistics.emission_percentage.gas_perc;
+	electricity = result.data.statistics.emission_percentage.electricity_perc;
+	landTransport = result.data.statistics.emission_percentage.transport_perc.land_perc;
+	plane = result.data.statistics.emission_percentage.transport_perc.air_perc;
+	trees = result.data.offsets[0].minTrees;
+	carDistance = result.data.statistics.equivalences[0].carDistance;
+	carbonWeight = (result.data.statistics.carbon_footprint.total / 1000).toFixed(2);
+	console.log(gas, electricity, landTransport, plane, trees, carDistance);
 }
+
+const options = {};
 
 getData();
 
@@ -38,29 +87,70 @@ getData();
 
 const FootprintPage: React.FC = () => {
 	const { carbonData } = useUserContext();
+	const [carbonWeight, setCarbonWeight] = React.useState<string>("");
+	const [graphData, setGraphData] = React.useState({
+		labels: [],
+		datasets: [
+			{
+				data: [0, 0, 0, 0], // Initialize with default values
+				backgroundColor: ["#fc881c", "#00d856", "#fbff08", "#560bf5"],
+				borderColor: "",
+				borderWidth: 0,
+			},
+		],
+	});
+
+	React.useEffect(() => {
+		async function fetchData() {
+			try {
+				const result: CarbonFootprintData = await getCarbonFootprint();
+				// Update state with fetched data
+				setGraphData({
+					labels: [],
+					datasets: [
+						{
+							data: [
+								result.data.statistics.emission_percentage.gas_perc,
+								result.data.statistics.emission_percentage.electricity_perc,
+								result.data.statistics.emission_percentage.transport_perc.land_perc,
+								result.data.statistics.emission_percentage.transport_perc.air_perc,
+							],
+							backgroundColor: ["#fc881c", "#00d856", "#fbff08", "#560bf5"],
+							borderColor: "",
+							borderWidth: 0,
+						},
+					],
+				});
+				setCarbonWeight((result.data.statistics.carbon_footprint.total / 1000).toFixed(2));
+				console.log(
+					result.data.statistics.emission_percentage.gas_perc,
+					result.data.statistics.emission_percentage.electricity_perc,
+					result.data.statistics.emission_percentage.transport_perc.land_perc,
+					result.data.statistics.emission_percentage.transport_perc.air_perc,
+				);
+			} catch (error) {
+				console.error("Error fetching data:", error);
+			}
+		}
+
+		if (carbonData !== null) {
+			fetchData();
+		}
+	}, [carbonData]);
 
 	if (carbonData === null) {
 		return null;
 	}
 
-	// const { carbonOffset } = carbonData;
-
-	// const gas = carbonOffset?.statistics?.carbon_footprint?.gas ?? 0;
-	// console.log(gas);
-	// const electricity = carbonOffset?.statistics?.carbon_footprint?.electricity ?? 0;
-	// const landTransport = carbonOffset?.statistics?.carbon_footprint?.transport?.land ?? 0;
-	// console.log(landTransport);
-	// const plane = carbonOffset?.statistics?.carbon_footprint?.transport?.air ?? 0;
-
 	return (
 		<div className="pt-20 flex flex-col  w-full items-center">
 			<div className="shadow-lg rounded-lg w-3/4 md:w-1/2 text-center mt-10 p-10 border-solid border-2 border-emerald-500">
 				<h2 className="text-2xl font-bold">Tu huella es:</h2>
-				<p className="text-8xl font-bold py-10 text-emerald-500">99,9</p>
+				<p className="text-8xl font-bold py-10 text-emerald-500">{carbonWeight}</p>
 				<h2 className="text-2xl font-bold  text-emerald-500">Toneladas de CO2 al año</h2>
 			</div>
 			<div className="flex justify-center items-center py-10 ">
-				<RadialDonut data={data} options={options} />
+				<RadialDonut data={graphData} options={options} />
 			</div>
 			<div className="flex flex-col items-start w-3/4 md:w-1/2">
 				<h2 className="text-2xl font-bold text-starts">Distribución:</h2>
